a5a895ea4841a83dff70e2f37922cc28102b7a4e
Irina Pinchuk <irvpi2015@hotmail.com>
2019-05-07 20:16:40 +0200

    [2-2-2] First function

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $lots = [
 
 // Пишем первую функцию
 
-function sumform($price) {
+function format_price($price) {
         $price = ceil($price);
         if ($price >= 1000) {
             $price = number_format($price, 0, '' ,' ').' ₽';
@@ -134,7 +134,7 @@ function sumform($price) {
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=sumform($lot['price']); ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_price($lot['price']); ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:25
