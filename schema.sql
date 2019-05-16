4cc4a1c8d5b08c0357e978fe67e75187977db637
Irina Pinchuk <irvpi2015@hotmail.com>
2019-05-16 06:34:18 +0200

    4-1-2 [Создание схемы БД личного проекта]

--- schema.sql
+++ schema.sql
@@ -3,55 +3,60 @@ DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 USE YetiCave;
 
+/**
+  Таблица категорий
+ */
 CREATE TABLE categories (
-	key_id	INT AUTO_INCREMENT PRIMARY KEY,
+	id	INT AUTO_INCREMENT PRIMARY KEY,
 	name	CHAR(128) NOT NULL UNIQUE,
 	code	CHAR(64) NOT NULL UNIQUE
 );
 
-INSERT INTO categories
-SET name = 'Доски и лыжи', code = 'boards';
-INSERT INTO categories
-SET name = 'Крепления', code = 'attachment';
-INSERT INTO categories
-SET name = 'Ботинки', code = 'boots';
-INSERT INTO categories
-SET name = 'Одежда', code = 'clothing';
-INSERT INTO categories
-SET name = 'Инструменты', code = 'tools';
-INSERT INTO categories
-SET name = 'Разное', code = 'other';
-
+/**
+  Таблица зарегестрированных пользователей
+ */
 CREATE TABLE users (
-	key_id		INT AUTO_INCREMENT PRIMARY KEY,
+	id		INT AUTO_INCREMENT PRIMARY KEY,
 	dt_add 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	email 		CHAR(128) NOT NULL UNIQUE,
 	name		CHAR(64) NOT NULL,
 	password	CHAR(64) NOT NULL,
 	avatar_path	CHAR(255),
 	info		CHAR(255) NOT NULL,
-	lot_id		INT,
-	rate_id		INT
+	INDEX user_email (email)
 );
 
+/**
+  Таблица лотов
+ */
 CREATE TABLE lots (
-	key_id		INT AUTO_INCREMENT PRIMARY KEY,
+	id		INT AUTO_INCREMENT PRIMARY KEY,
 	dt_add		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	name 		CHAR(255) NOT NULL UNIQUE,
 	descr 		TEXT(65535) NOT NULL,
 	img_url		CHAR(255),
-	price		INT NOT NULL,
+	start_price		INT UNSIGNED NOT NULL,
 	dt_fin		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	rate_step	INT NOT NULL,
-	cat_id		INT,
-	autor_id	INT,
-	winner_id	INT
+	cat_id		INT NOT NULL,
+	author_id	INT NOT NULL,
+	winner_id	INT NOT NULL,
+	FOREIGN KEY (author_id) REFERENCES users(id),
+	FOREIGN KEY (winner_id) REFERENCES users(id),
+	FOREIGN KEY (cat_id) REFERENCES categories(id),
+	INDEX lot_index (name, cat_id)
 );
 
+/**
+  Таблица ставок
+ */
 CREATE TABLE rates (
-	key_id	INT AUTO_INCREMENT PRIMARY KEY,
+	id	INT AUTO_INCREMENT PRIMARY KEY,
 	dt_add 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	price	INT NOT NULL,
-	user_id	INT,
-	lot_id	INT
-);
+	sum_rate	INT UNSIGNED NOT NULL,
+	user_id	INT NOT NULL,
+	lot_id	INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (lot_id) REFERENCES lots(id),
+	INDEX user_lot (user_id, lot_id)
+	);
